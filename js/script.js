3dbb8efb69ba27513896a8d030997003383f1537
Matías Carrillo <42281695+matiascc9911@users.noreply.github.com>
2025-05-17 20:06:13 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 document.querySelector('.prov-cont').addEventListener('click', function (e) {
-    if (e.target.closest('.imgs__contenedor')) return;
+    if (e.target.closest('.imgs__contenedor, a')) return;
     const track = this.querySelector('.sect-prov');
     const isPaused = track.style.animationPlayState === 'paused';
     track.style.animationPlayState = isPaused ? 'running' : 'paused';
